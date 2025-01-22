5d077ee57a72e18f882760578a55601bf9a85965
Super Rama <superrama86@gmail.com>
2025-01-22 21:10:06 +0000

    Correction et commenté le code valid.js

--- valid.js
+++ valid.js
@@ -4,29 +4,37 @@ const Form = document.getElementById('registrationForm');
 
 Form.addEventListener("submit", function (event) {
     event.preventDefault();
+    // TODO : Recupéré les éléments du html
+    const userInputElement = document.getElementById('userinput')
+    const usermailElement = document.getElementById('usermail')
+    const userpasswordElement = document.getElementById('userpassword')
+
+    // TODO: Obtenir les valeurs entréés
     const userInput = document.getElementById('userinput').value.trim();
     const usermail = document.getElementById('usermail').value.trim();
     const userpassword = document.getElementById('userpassword').value.trim();
     const errorMessage = document.getElementById('errorMessage');
+    // TODO : Initialisé le message d'erreur
     errorMessage.textContent = '';
+        // TODO : Les conditions imposées pour valider le formulaire
     if(userInput === "" || usermail === "" || userpassword === ""){
         errorMessage.textContent = 'Merci de remplir tous les champs!';
-    
+        return;
     };
 
     if(userpassword.length < 8){
         errorMessage.textContent = 'Veuillez mettre un mot de passe de 8 caractères';
-        event.preventDefault();
+        return;
     }else if(!/[A-Z]/.test(userpassword)){
         errorMessage.textContent = 'Votre mot de passe doit contenir au moins une lettre majuscule'
-    }else if(!/[a-]/.test(userpassword)){
+    }else if(!/[a-z]/.test(userpassword)){
         errorMessage.textContent = 'Votre mot de passe doit contenir au moins une lettre minuscule'
     }else if(!/[0-9]/.test(userpassword)){
         errorMessage.textContent = 'Votre mot de passe doit contenir au moins un chiffre'
-        event.preventDefault();
+        return;
     }else if(!/[@~#$^!&*]/.test(userpassword)){
         errorMessage.textContent = 'Votre mot de passe doit contenir au moins un caractère spécial'
-        event.preventDefault();
+        return;
     }else{
         errorMessage.textContent = '';
         alert('Formulaire soumis avec succès !')
@@ -37,8 +45,9 @@ Form.addEventListener("submit", function (event) {
         errorMessage.textContent = 'Veuillez saisir un email correcte';
         event.preventDefault(); //Empêche la soumission du formulaire
     }
-    userInput.value = '';
-    usermail.value = '';
-    userpassword = '';
+    // TODO: Vider les champs
 
+    userInputElement.value = "";
+    usermailElement.value = "";
+    userpasswordElement.value = "";
 });
